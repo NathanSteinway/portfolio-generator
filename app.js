d19fdc06cca5cdeb9465bd7595d36f5039fb611f
ganbari <steinway.nathan@gmail.com>
2022-01-24 10:38:49 -0600

    prevented user from moving on without providing required values

--- app.js
+++ app.js
@@ -16,12 +16,28 @@ const promptUser = () => {
         {
             type: 'input',
             name: 'name',
-            message: 'What is your name?'
+            message: 'What is your name? (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('please enter name');
+                    return false;
+                }
+            }
           },
           {
             type: 'input',
             name: 'github',
-            message: 'Enter your GitHub Username'
+            message: 'Enter your GitHub Username',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('please enter username');
+                    return false;
+                }
+            }
           },
           {
             type: 'input',
@@ -49,12 +65,28 @@ const promptProject = portfolioData => {
         {
             type: 'input',
             name: 'name',
-            message: 'What is the name of your project?'
+            message: 'What is the name of your project?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('please enter project name');
+                    return false;
+                }
+            }
           },
           {
             type: 'input',
             name: 'description',
-            message: 'Provide a description of the project (Required)'
+            message: 'Provide a description of the project (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('please enter description');
+                    return false;
+                }
+            }
           },
           {
             type: 'checkbox',
@@ -65,7 +97,15 @@ const promptProject = portfolioData => {
           {
             type: 'input',
             name: 'link',
-            message: 'Enter the GitHub link to your project. (Required)'
+            message: 'Enter the GitHub link to your project. (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log('please enter link');
+                    return false;
+                }
+            }
           },
           {
             type: 'confirm',
